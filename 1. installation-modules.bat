69fc1f4cc7393a510652a23ec2359d8560c193e2
aqwa <Aqwa1200@gmail.com>
2019-05-07 14:37:34 +0200

    Add files via upload

--- 1. installation-modules.bat
+++ 1. installation-modules.bat
@@ -1,5 +1,7 @@
 @echo off
 echo.Installation des dependances
+set /P userchoice="Souhaitez vous installer les dependances (O/N) ? "
+if /i NOT "%userchoice%"=="o" goto:eof
 start "" /B npm install -- save discord.js
 start "" /B npm install -- save os
 start "" /B npm install -- save cpu-stat
@@ -10,4 +12,3 @@ start "" /B npm install -- save chalk
 start "" /B npm install -- save physical-cpu-count
 start "" /B npm install -- save figlet
 start "" /B npm install -- save chalk-animation
-PAUSE>nul 2>nul
